4d8e540b42cd0d93d66d3052d1435e9c17ba1e25
Nehan Chandira <136231115+WANehan923@users.noreply.github.com>
2023-07-31 20:34:47 +0530

    Add files via upload

---
+++ C# Lab 04/C# Q-1.3/C# Q-1.3/ConvertValues.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Q_1._3
+{
+    internal class ConvertValues
+    {
+        public double KilometerTOMeter (double kilometer)
+        {
+            double meter = kilometer * 1000;
+            return meter;
+        }
+    }
+}
