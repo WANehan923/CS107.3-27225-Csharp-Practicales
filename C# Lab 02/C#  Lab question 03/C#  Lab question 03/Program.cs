7d111a7ec234bd616abe5ac8f3648de55b1bf2a8
Nehan Chandira <136231115+WANehan923@users.noreply.github.com>
2023-07-31 20:30:44 +0530

    Add files via upload

---
+++ C# Lab 02/C#  Lab question 03/C#  Lab question 03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C___Lab_question_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius;
+            double circumference;
+            Console.WriteLine("Enter the value of radius:");
+            radius = double.Parse(Console.ReadLine());
+            circumference = 2 * Math.PI * radius;
+            Console.WriteLine("Circumference:" + Math.Round(circumference, 2));
+
+
+        }
+    }
+}
